22a31e94df43c9cc715fa540c44167bc23410e2c
mithrayls <mithray@protonmail.com>
2020-08-02 18:25:49 +1200

    hashing files

--- test.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-
-const arr = [ 'one', 'two', 'three', 'four', 'one', 'two', 'four', 'three' ]
-arr.forEach(word=>{
-    var newHash = crypto.createHash('sha256')
-    newHash.update(word)
-    console.log(newHash.digest('hex'))
-})
