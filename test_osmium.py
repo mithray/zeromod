b8ac8badb7f092aeebb73e5c0b8c8743a41cfc63
mithrayls <mithray@protonmail.com>
2020-08-12 05:01:05 +1200

    berry regen working, but need modified component ResourceSupply.js from borg, and disable resource destruction(in some cases, but not all!)

---
+++ test_osmium.py
@@ -0,0 +1,40 @@
+"""
+Compute the total length of highways in an osm file.
+Shows how to extract the geometry of a way.
+"""
+import osmium as o
+import sys
+
+class RoadLengthHandler(o.SimpleHandler):
+    def __init__(self):
+        super(RoadLengthHandler, self).__init__()
+        self.length = 0.0
+
+    def way(self, w):
+        print(w)
+        if 'highway' in w.tags:
+            try:
+                added_length = o.geom.haversine_distance(w.nodes)
+                self.length += added_length
+                print('added_length: %.2f km' % (added_length/1000))
+            except o.InvalidLocationError:
+                # A location error might occur if the osm file is an extract
+                # where nodes of ways near the boundary are missing.
+                print("WARNING: way %d incomplete. Ignoring." % w.id)
+
+def main(osmfile):
+    h = RoadLengthHandler()
+    # As we need the geometry, the node locations need to be cached. Therefore
+    # set 'locations' to true.
+    h.apply_file(osmfile, locations=True)
+
+    print('Total way length: %.2f km' % (h.length/1000))
+
+    return 0
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python %s <osmfile>" % sys.argv[0])
+        sys.exit(-1)
+
+    exit(main(sys.argv[1]))
