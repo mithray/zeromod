7eda5b34edd5a4c77899244a9bb53bdaf6842ed9
mithrayls <mithray@protonmail.com>
2020-08-19 04:05:41 +1200

    get civs

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,21 +1,31 @@
 const fs = require('fs')
 const YAML = require('yaml')
 
+function newLine2Array (val, parent) {
+    val = val.trim()
+    if(val.includes('\n')){
+        val = val.split("\n")
+        val = val.map(s => s.trim());
+    }
+
+    return val
+}
+
 function parse(inputPath){
     var jsobj
-    var data = fs.readFileSync(inputPath,{encoding: 'utf-8'})
-
-    var newLine2Array = function(val, parent) {
-        val = val.trim()
-        if(val.includes('\n')){
-            val = val.split("\n")
-            val = val.map(s => s.trim());
-        }
-        return val
+    var data
+    try {
+        data = fs.readFileSync(inputPath,{encoding: 'utf-8'})
+    } catch (e){
+        data = fs.readFileSync(inputPath,{encoding: 'utf-16'})
     }
 
     if (inputPath.endsWith('.json')){
-        jsobj = JSON.parse(data)
+       try{
+           jsobj = JSON.parse(data)
+        } catch(e) {
+            console.log(e)
+        }
     }
     if (inputPath.endsWith('.xml')){
         let options = { tagValueProcessor:  newLine2Array}
