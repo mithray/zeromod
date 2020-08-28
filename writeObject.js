b5cd8990d3ea8181b329266ae8c1a1f38b294a61
mithrayls <mithray@protonmail.com>
2020-08-29 04:06:17 +1200

    try to get at least basic functionality

---
+++ writeObject.js
@@ -0,0 +1,9 @@
+const parser = require('fast-xml-parser')
+const fs = require('fs')
+const path = require('path')
+
+function writeObject( options = { obj, outputPath } ){
+	
+}
+
+module.exports = writeObject
