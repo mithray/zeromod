708b5e6cf474f1c6099aa4ebdbe2dae4ad341474
mithrayls <mithray@protonmail.com>
2020-08-30 06:27:46 +1200

    building dataset form files in a directory recursively

--- create_civil_centres.js
+++ create_civil_centres.js
@@ -18,3 +18,15 @@ console.log(civ)
     let interpolated = interpolate(template,civ)
 	console.log(interpolated)
 }
+var cache = [];
+JSON.stringify(circ, (key, value) => {
+  if (typeof value === 'object' && value !== null) {
+    // Duplicate reference found, discard key
+    if (cache.includes(value)) return;
+
+    // Store value in our collection
+    cache.push(value);
+  }
+  return value;
+});
+cache = null
