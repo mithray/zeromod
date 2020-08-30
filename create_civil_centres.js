5b96a3c1e8be2ae81f90528880d8bbb8a13389a3
mithrayls <mithray@protonmail.com>
2020-08-31 05:10:50 +1200

    building dataset form files in a directory recursively

--- create_civil_centres.js
+++ create_civil_centres.js
@@ -1,23 +1,41 @@
 const fs = require('fs')
-const getObjectsFromPath = require('./helpers/getObjectsFromPath.js') 
+const path = require('path')
+const buildObjectFromPath = require('./helpers/buildObjectFromPath.js') 
 const interpolate = require('./helpers/interpolate.js')
+const changeCase = require('change-case')
 
-const civs = [
+/*
+*/
+
+async function createCivilCentres(){
+const target_civs = [
 	"athen",
 	"brit",
 	"gaul"
 ]
-
-getObjectsFromPath('./config/civilizations')
-
-for ( let i = 0; i < civs.length; i ++ ){
-	civ = civs[i]
-
+//console.log(target_civs)
+  const config = await buildObjectFromPath(path.join(process.cwd(),'./config'))
+  const template = ''
+  const civs = config.civilizations
+  const keys = Object.keys(civs)
+//    console.log(keys)
+//    console.log(civs)
+  for ( let i = 0; i < keys.length; i ++ ){
+      
+    const civ = civs[keys[i]]
+//console.log(civ.code)
+//console.log(target_civs)
+    if( target_civs.includes(civ.code) ){
 console.log(civ)
-	let template = getObjectsFromPath('civil_centre.yml')
-    let interpolated = interpolate(template,civ)
-	console.log(interpolated)
+    }
+//  console.log(keys[i])
+//	civ = civs[i]
+//    let interpolated = interpolate(template,civ)
+//	console.log(interpolated)
+  }
 }
+createCivilCentres()
+/*
 var cache = [];
 JSON.stringify(circ, (key, value) => {
   if (typeof value === 'object' && value !== null) {
@@ -30,3 +48,4 @@ JSON.stringify(circ, (key, value) => {
   return value;
 });
 cache = null
+*/
