c8c79e61689dac20277c99c91ae95a2a14ab3eff
mithrayls <mithray@protonmail.com>
2020-09-02 18:20:41 +1200

    civic centre alg completed

--- create_civil_centres.js
+++ create_civil_centres.js
@@ -1,14 +1,20 @@
 const fs = require('fs')
+var pretty = require('pretty')
 const path = require('path')
 const buildObjectFromPath = require('./helpers/buildObjectFromPath.js') 
 const interpolate = require('./helpers/interpolate.js')
 const changeCase = require('change-case')
+const snake = changeCase.snakeCase
+const pascal = changeCase.pascal
+const camel = changeCase.camel
 const merge = require('deepmerge')
 const truePhase = true
 var config = {}
 
 function buildUnitTree(structureTree){
   const buildTree = {}
+//console.log('structureTree')
+//console.log(structureTree)
   delete structureTree.classes
   const keys = Object.keys(structureTree)
   for (let i = 0 ; i < keys.length; i++){
@@ -26,8 +32,9 @@ function buildUnitTree(structureTree){
 
 function buildStructureTree(tree){
   const trees = []
+
   if (typeof tree === 'string'){
-    tree = config.buildTrees[tree]
+    tree = config.build_trees[tree]
   } 
   if (typeof tree === 'object'){
     if (tree.classes){
@@ -44,47 +51,86 @@ function buildStructureTree(tree){
 
 async function createCivilCentres(){
   config = await buildObjectFromPath(path.join(process.cwd(),'./config'))
+  var templates = []
+  
+  config.active_mod = config.ars_bellica
   const target_civs = [
     "athen",
     "brit",
     "gaul",
 "han"
   ]
-  const template = ''
   const civs = config.civilizations
-console.log(civs)
   const keys = Object.keys(civs)
-  console.log('keys')
-  console.log(keys)
   for ( let i = 0; i < keys.length; i ++ ){
       
     const civ = civs[keys[i]]
-console.log(target_civs)
-    if( target_civs.includes(civ.Code) ){
-console.log(civ.Build)
-      const structureTree = buildStructureTree(civ.BuildTree)
-      const buildTree = buildUnitTree(structureTree)
-      civ.buildTree = buildTree
+    if( target_civs.includes(civ.code) ){
+      const structureTree = buildStructureTree(civ.build_tree)
+      civ.build_tree = buildUnitTree(structureTree)
       const tmp = await buildObjectFromPath(path.join(process.cwd(),'./civil_centre.yml'))
-console.log('1---')
-console.log(tmp)
-console.log('2---')
-console.log(civ)
-console.log('3---')
-      const ccTemplate = interpolate(tmp.civil_centre, civ)
-  //    console.log(buildTree)
-//xmlData = convert.json2xml(JSON.stringify(ccTemplate), options);
+      config.civ = civ
+      ccTemplate = interpolate(tmp.civil_centre, config)
+      ccTemplate.Entity.Identity.Classes = '\n\t\t'+ccTemplate.Entity.Identity.Classes.join('\n\t\t')+'\n'
+      ccTemplate.Entity.ProductionQueue.Technologies.__text = '\n\t\t'+ccTemplate.Entity.ProductionQueue.Technologies.__text.replace(/,/g,'\n\t\t')+'\n'
+      ccTemplate.Entity.ProductionQueue.Entities.__text = '\n\t\t'+ccTemplate.Entity.ProductionQueue.Entities.__text.toLowerCase().replace(/,/g,'\n\t\t')+'\n'
+        const phase_names = config.active_mod.phases.names
+      if( config.active_mod.phases.type === 'phase' ){
+        for ( let i = 0; i < phase_names.length; i++ ){
+          var template = ccTemplate
+          if( i+1 < phase_names.length){
+            promotion = {
+              RequiredXp: 10000,
+              Entity: `structures/athenian/civil_centre_${phase_names[i+1]}`
+            } 
+          } else {
+            promotion = { "_disable": "" }
+          }
+          if( i > 0 ){
+            template = {
+              Entity: {
+                Identity: {
+                  Classes: {
+                    _datatype: "tokens",
+                    __text: phase_names[i] + "Centre"
+                  },
+                  VisibleClasses: {
+                    _datatype: "tokens",
+                    __text: phase_names[i] + "Centre"
+                  }
+                }
+              }
+            }
+            template.fileName = civ.code + '_civil_centre_' + phase_names[i]
+          } else {
+            template.fileName = civ.code + '_civil_centre'
+          }
+          if(i===1){
+            template.Entity._parent = `structures/${civ.code}_civil_centre`
+          }
+          if(i>1){
+            template.Entity._parent = `structures/${civ.code}_civil_centre_${phase_names[i-1]}`
+          }
+        template.Entity.Identity.GenericName = changeCase.capitalCase(phase_names[i]) + " Centre",
+        template.Entity.Promotion = promotion
+        templates.push(template)
+        }
+  //console.log(templates)
+      } else {
+        var template = ccTemplate
+        template.Entity.Identity.GenericName = "Civil Centre"
+        templates.push(template)
+      }
 
-
-const X2JS = require('./x2js.js')
-
-var x2js = new X2JS()
-var xml = x2js.json2xml_str( ccTemplate )
-
-console.log('<?xml version="1.0" encoding="utf-8"?>\n'+xml)
-
-
-
+      const X2JS = require('./x2js.js')
+      var x2js = new X2JS()
+      for( let j = 0 ; j < templates.length; j++ ){
+var fileName = templates[j].fileName
+delete templates[j].fileName
+        var xml = pretty(x2js.json2xml_str( templates[j] ))
+        xml = '<?xml version="1.0" encoding="utf-8"?>\n'+xml
+       fs.writeFileSync(path.join(process.cwd(),'dist/ars_bellica/simulation/templates/structures/',fileName +'.xml'),xml)
+      }
     }
 
   }
--- helpers/interpolate.js
+++ helpers/interpolate.js
@@ -2,22 +2,24 @@ const safeEval = require('safe-eval')
 
 function interpolate(template, data){
 
-console.log('data')
-console.log(data)
-console.log('/data')
-console.log(template)
-
+/*
+*/
   function interp(match, p1 ){
-console.log(match)
-    const context = data
-    const evaled = safeEval(p1,context)
+//console.log(match)
+    var evaled = safeEval(p1,data)
+//console.log(data)
+    const evalString = JSON.stringify(evaled)
+//console.log(evalString)
+    if(evalString.match(/\$\{.*\}/)){
+      evaled = JSON.parse(interpolate(evalString,data))
+    }
     return evaled
   }
 
   const tmp = JSON.stringify(template).replace(/\$\{(.*?)\}/g, interp)
   const json = JSON.parse(tmp)
 
-console.log(json)
+//console.log(json)
   return json
 	
 }
